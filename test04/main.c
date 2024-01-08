1b3f66b8ce3b5884beb81b6efd92915b5512224a
André L Josefino <ALJosefino@gmail.com>
2024-01-07 23:15:14 -0300

    Multiline Comments
    All text between the /* and the */ is ignored. The Code::Blocks editor displays commented text in a unique color, which further confirms how the
    compiler sees and ignores the comment text.

---
+++ test04/main.c
@@ -0,0 +1,13 @@
+/* Author: Dan Gookin
+ This program displays text on the screen */
+
+#include <stdio.h> /* Required for puts() */
+
+    int main()
+        {
+
+            puts("Greetings, human."); /* Displays text */
+
+            return 0;
+
+        }
