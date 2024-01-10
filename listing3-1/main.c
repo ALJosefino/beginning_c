675ff0c35a1962a36cbe4157d476d4ce46b59e9d
André L Josefino <ALJosefino@gmail.com>
2024-01-09 22:23:45 -0300

    Listing 3-1: Overly Commented Source Code
    You can see comments in Listing 3-1. A comment can appear on a line by
    itself or at the end of a line.

---
+++ listing3-1/main.c
@@ -0,0 +1,16 @@
+
+    /* Author: Dan Gookin */
+    /* This program displays text on the screen */
+
+    #include <stdio.h>
+
+    /* Required for puts() */
+    int main()
+
+        {
+
+            puts("Greetings, human.");
+
+            return 0;
+
+        }
