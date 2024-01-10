c704eeb15e26550b27c98599d4772378277bc1c2
André L Josefino <ALJosefino@gmail.com>
2024-01-09 22:28:42 -0300

    Listing 3-2: Multiline Comments
    The first two lines can be combined for a multiline comment, as shown in
    Listing 3-2.
    
    All text between the /* and the */ is ignored.

---
+++ listing3-2/main.c
@@ -0,0 +1,15 @@
+
+    /* Author: Dan Gookin
+    This program displays text on the screen */
+    #include <stdio.h>
+    /* Required for puts() */
+
+    int main()
+
+        {
+
+            puts("Greetings, human.");
+
+            return 0;
+
+        }
