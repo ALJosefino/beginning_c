3889179fd843f7dc2edaa17b63ce8f610298742e
André L Josefino <ALJosefino@gmail.com>
2024-01-10 22:53:16 -0300

    Listing 4-3:   Disabling a Statement
    Commenting out a statement
    Comments are used to not only add information, remarks, and descriptions
    to your source code but also disable statements, as shown in Listing 4-3.

---
+++ listing4-3/main.c
@@ -0,0 +1,13 @@
+
+        #include <stdio.h>
+
+            int main()
+
+                {
+
+                    puts("The secret password is:");
+                    /* puts(“Spatula.”); */
+
+                    return(0);
+
+                }
