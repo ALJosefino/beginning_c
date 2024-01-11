1563a76d0bd58d27ab8223e92624321cd82e42e3
André L Josefino <ALJosefino@gmail.com>
2024-01-10 23:13:21 -0300

    Listing 4-6: Another Horrible Mistake
    Goofing up on purpose again
    The section “Goofing up on purpose,” earlier in this chapter, introduces you
    to compiler errors. The complier is only part of the program creation pro-
    cess. The other major part is linking, and, yes, you’ll find that the linker can
    detect errors as well, as shown in Listing 4-6.

---
+++ listing4-5/main.c
@@ -0,0 +1,12 @@
+
+        #include <stdio.h>
+
+        int main()
+
+            {
+
+                printf("I have been a stranger in a strange land.");
+
+                return(0);
+
+            }
---
+++ listing4-6/main.c
@@ -0,0 +1,12 @@
+
+    #include <stdio.h>
+
+        int main()
+
+        {
+
+            writeln("Another horrible mistake.");
+
+            return(0);
+
+        }
