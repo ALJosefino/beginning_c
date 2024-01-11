9bcb66f9154dbc549faf7faba5d043155cbedae0
André L Josefino <ALJosefino@gmail.com>
2024-01-11 13:32:44 -0300

    Listing 5-3: Another Immediate Math Problem
    Reviewing the float-integer thing
    The difference between an immediate value being a float or an integer is how
    you specify it in a program. Consider Listing 5-3.
    
    The values 16 and 17 are integers; they have no decimal part.

---
+++ listing5-3/main.c
@@ -0,0 +1,13 @@
+
+
+        #include <stdio.h>
+
+            int main()
+
+                {
+
+                    printf("The total is %d\n",16+17);
+
+                    return(0);
+
+                }
