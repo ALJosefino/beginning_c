b8c691d13175e5f72ad5e99085f61f858073fa1d
André L Josefino <ALJosefino@gmail.com>
2024-01-12 21:10:59 -0300

    Listing 6-5: Declaring a Variable and Assigning a Value
    Assigning a value upon creation
    C programmers love to load up code, often putting way too much stuff in
    a single statement. You can do that in C, but it often obfuscates the code,
    making it more difficult to read. Still, you can load up the code in ways that
    simply save time.
    In Listing 6-5, the integer variable start is created and assigned the value 0
    upon creation. This combination saves typing another line of code that would
    assign 0 to the start variable.

---
+++ listing6-5/main.c
@@ -0,0 +1,14 @@
+
+        #include <stdio.h>
+
+            int main()
+
+                {
+
+                        int start = 0;
+
+                        printf("The starting value is %d.\n", start);
+
+                        return(0);
+
+                }
